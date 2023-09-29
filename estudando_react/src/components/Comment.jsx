d57963187284cf856c8874273f6f86f425f8700c
Emanuel <emanuel.gui.santana@gmail.com>
2023-09-29 14:58:58 -0300

    wip

--- estudando_react/src/components/Comment.jsx
+++ estudando_react/src/components/Comment.jsx
@@ -1,3 +1,4 @@
+import { ThumbsUp, Trash } from '@phosphor-icons/react'
 import styles from './Comment.module.css'
 
 export function Comment() {
@@ -7,14 +8,26 @@ export function Comment() {
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
-          <haeder></haeder>
+          <haeder>
+            <div className={styles.authorAndTime}>
+              <strong>Emanuel Guimarães Santana</strong>
+              <time title="11 de maio às 08:13"dateTime="2023-09-19 14:05:49"> Cerca de 1h atras</time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={20}/>
+            </button>
+          </haeder>
 
           <p>
-            
+          Muito bom Devon, parabéns!! 👏👏
           </p>
         </div>
         <footer>
-          aplaudir
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
         </footer>
       </div>
     </div>
